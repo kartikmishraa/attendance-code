a67b714f564f54862e1035251039cad6a8eb9539
Kartik Mishra <kartikm2219@gmail.com>
2023-10-26 18:27:24 +0530

    updated constants

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student } from 'src/shared/models/interfaces/Student';
+import { STUDENT_ENDPOINT } from 'src/shared/models/constants/api.constant';
 
 @Injectable({
   providedIn: 'root',
@@ -10,14 +11,14 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   // Store this in a CONSTANTS file?
-  apiUrl = 'https://65390885e3b530c8d9e7ca59.mockapi.io/api/student/';
+  // apiUrl = 'https://65390885e3b530c8d9e7ca59.mockapi.io/api/student/';
 
   /**
    * @description GET /api/student
    * @returns Student Data from the backend
    */
   getAllStudents(): Observable<Student[]> {
-    return this.http.get<Student[]>(this.apiUrl);
+    return this.http.get<Student[]>(STUDENT_ENDPOINT);
   }
 
   /**
@@ -26,6 +27,6 @@ export class DataService {
    * @returns Student Data that was uploaded
    */
   addOneStudent(data: Student): Observable<Student> {
-    return this.http.post<Student>(this.apiUrl, data);
+    return this.http.post<Student>(STUDENT_ENDPOINT, data);
   }
 }
---
+++ src/shared/models/constants/api.constant.ts
@@ -0,0 +1,4 @@
+export const STUDENT_ENDPOINT =
+  'https://65390885e3b530c8d9e7ca59.mockapi.io/api/student';
+export const ATTENDANCE_ENDPOINT =
+  'https://65390885e3b530c8d9e7ca59.mockapi.io/api/attendance';
