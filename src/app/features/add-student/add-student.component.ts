cc6d50d08480a6ba019376318c222af4d093015e
Kartik Mishra <kartikm2219@gmail.com>
2023-10-26 10:18:14 +0530

    comments

--- src/app/features/add-student/add-student.component.ts
+++ src/app/features/add-student/add-student.component.ts
@@ -9,9 +9,31 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class AddStudentComponent {
   constructor(private fb: FormBuilder) {}
 
+  /**
+   * @description: FormGroup for controlling the form
+   */
   addStudentForm = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
-    phone: ['', Validators.required],
+    phone: ['', Validators.required, Validators.pattern('^[0-9]{10}$')],
   });
+
+  /**
+   * @description: Function that handles form submission
+   */
+  handleSubmit(): void {
+    if (this.addStudentForm.valid) {
+      console.log('form submitted');
+      console.log(this.addStudentForm.value);
+    } else {
+      console.log('invalid form');
+    }
+  }
+
+  /**
+   * @description: Function to reset form values
+   */
+  handleReset(): void {
+    this.addStudentForm.reset();
+  }
 }
