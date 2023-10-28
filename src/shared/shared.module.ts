bb7592332b54c6beb1fe4fc64c759262b19f99c5
Kartik Mishra <kartikm2219@gmail.com>
2023-10-28 18:19:49 +0530

    updated imports

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
